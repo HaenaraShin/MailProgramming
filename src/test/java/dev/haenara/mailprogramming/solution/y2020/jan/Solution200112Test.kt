07abf16a77d2fe69dd9dc8c556c1fc2153c6baeb
HaenalaShin <hamster12345@gmail.com>
2020-01-15 19:33:02 +0900

    🔎 2020.01.12 테스트코드 추가
    예제1번 입력 일 때 예제1번 결과
    예제2번 입력 일 때 예제2번 결과

---
+++ src/test/java/dev/haenara/mailprogramming/solution/y2020/jan/Solution200112Test.kt
@@ -0,0 +1,22 @@
+package dev.haenara.mailprogramming.solution.y2020.jan
+
+import Solution200112
+import org.junit.Assert.*
+import org.junit.Test
+
+internal class Solution200112Test {
+
+    @Test
+    fun `20200112-TC1 예제1번 입력 일 때 예제1번 결과`() {
+        assertArrayEquals(
+            longArrayOf(120, 60, 40, 30, 24),
+            Solution200112().solution(intArrayOf(1, 2, 3, 4, 5)))
+    }
+
+    @Test
+    fun `20200112-TC2 예제2번 입력 일 때 예제2번 결과`() {
+        assertArrayEquals(
+            longArrayOf(1152, 1920, 1440, 2880, 960, 720),
+            Solution200112().solution(intArrayOf(5, 3, 4, 2, 6, 8)))
+    }
+}
